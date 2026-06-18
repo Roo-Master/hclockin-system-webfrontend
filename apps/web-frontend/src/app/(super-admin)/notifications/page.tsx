88783c1d5e97943df0c58d0141b664bdf48bfe27
EngDaudi <drmwangi40@gmail.com>
2026-06-18 13:39:41 +0300

    new

--- apps/web-frontend/src/app/(super-admin)/notifications/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(super-admin)/notifications/page.tsx
-import NotificationsPage from '@/components/super-admin/notifications/page';
-
-export default function Page() {
-  return <NotificationsPage />;
-}
--- apps/web-frontend/src/app/(super-admin)/profile/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(super-admin)/profile/page.tsx
-import ProfilePage from '@/components/super-admin/profile/page';
-
-export default function Page() {
-  return <ProfilePage />;
-}
